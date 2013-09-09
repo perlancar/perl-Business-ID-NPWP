47895a1e8a7e66174603a7eb277b162380ac801c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-09-09 16:45:02 +0700

    Incompatible change: replace old interface (validate_npwp + OO) with new Rinci-based parse_npwp

--- lib/Business/ID/NPWP.pm
+++ lib/Business/ID/NPWP.pm
@@ -1,272 +1,110 @@
 package Business::ID::NPWP;
+
+use 5.010001;
+use warnings;
+use strict;
+
+use DateTime;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(parse_npwp);
+
+# VERSION
+
+our %SPEC;
+
+sub _z { $_[0] > 9 ? $_[0]-9 : $_[0] }
+
+$SPEC{parse_npwp} = {
+    v => 1.1,
+    summary => 'Parse Indonesian taxpayer registration number (NPWP)',
+    args => {
+        npwp => {
+            summary => 'Input NPWP to be parsed',
+            schema  => 'str',
+            pos => 0,
+            req => 1,
+        },
+    },
+};
+sub parse_npwp {
+    my %args = @_;
+
+    my $npwp = $args{npwp} or return [400, "Please specify npwp"];
+    my $res = {};
+
+    $npwp =~ s/^\s+//;
+    # assume A = 0 if not specified
+    if ($npwp =~ /^\d\./) { $npwp = "0$npwp" }
+
+    $npwp =~ s/\D+//g;
+    # assume BBB = 000 if not specified
+    if (length($npwp) == 12) { $npwp .= "000" }
+    return [400, "Not 15 digit"] unless length($npwp) == 15;
+
+    $npwp =~ /^(.)(.)(.)(.)(.)(.)(.)(.)(.)/;
+    if ((_z(1*$1) + _z(2*$2) + _z(1*$3) +
+	 _z(2*$4) + _z(1*$5) + _z(2*$6) +
+         _z(1*$7) + _z(2*$8) + _z(1*$9)) % 10) {
+        return [400, "Wrong check digit"];
+    }
+
+    (
+        $res->{taxpayer_code}, $res->{serial}, $res->{check_digit},
+        $res->{tax_office_code}, $res->{branch_code},
+    ) = $npwp =~ /(..)(.{6})(.)(...)(...)/;
+
+    return [400, "Serial starts from 1, not 0"] if $res->{serial} < 1;
+
+    $res->{normalized} = join(
+        "",
+        $res->{taxpayer_code}, ".",
+        substr($res->{serial}, 0, 3), ".", substr($res->{serial}, 3), ".",
+        $res->{check_digit}, "-",
+        $res->{tax_office_code}, ".", $res->{branch_code},
+    );
+
+    [200, "OK", $res];
+}
+
+1;
 # ABSTRACT: Validate Indonesian taxpayer registration number (NPWP)
 
 =head1 SYNOPSIS
 
-    use Business::ID::NPWP;
+ use Business::ID::NPWP qw(parse_npwp);
 
-    # OO-style
+ my $res = parse_npwp(npwp => "02.183.241.5-000.000");
 
-    my $npwp = Business::ID::NPWP->new($str);
-    die "Invalid NPWP!" unless $npwp->validate;
-
-    print $npwp->taxpayer_code, "\n"; # also, kode_wajib_pajak()
-    print $npwp->serial, "\n"; # also, nomor_urut()
-    print $npwp->check_digit, "\n";
-    print $npwp->local_tax_office_code, "\n"; # also, kode_kpp()
-    print $npwp->branch_code, "\n"; # also, kode_cabang()
-
-    # procedural style
-
-    validate_npwp($str) or die "Invalid NPWP!";
 
 =head1 DESCRIPTION
 
-This module can be used to validate Indonesian taxpayer registration
-number, Nomor Pokok Wajib Pajak (NPWP).
+This module can be used to validate Indonesian taxpayer registration number,
+Nomor Pokok Wajib Pajak (NPWP).
 
 NPWP is composed of 15 digits as follow:
 
  ST.sss.sss.C-OOO.BBB
 
-C<S> is a serial number from 0-9 (so far haven't seen 7 and up, but
-it's probably possible).
+C<S> is a serial number from 0-9 (so far haven't seen 7 and up, but it's
+probably possible).
 
-C<T> denotes taxpayer type code (0 = government treasury [bendahara
-pemerintah], 1-3 = company/organization [badan], 4/6 = invidual
-entrepreneur [pengusaha perorangan], 5 = civil servants [pegawai
-negeri, PNS], 7-9 = individual employee [pegawai perorangan]).
+C<T> denotes taxpayer type code (0 = government treasury [bendahara pemerintah],
+1-3 = company/organization [badan], 4/6 = invidual entrepreneur [pengusaha
+perorangan], 5 = civil servants [pegawai negeri, PNS], 7-9 = individual employee
+[pegawai perorangan]).
 
-C<sss.sss> is a 6-digit serial code for the taxpayer, probably starts
-from 1. It is distributed in blocks by the central tax office (kantor
-pusat dirjen pajak, DJP) to the local tax offices (kantor pelayanan
-pajak, KPP) throughout the country for allocation to taypayers.
+C<sss.sss> is a 6-digit serial code for the taxpayer, probably starts from 1. It
+is distributed in blocks by the central tax office (kantor pusat dirjen pajak,
+DJP) to the local tax offices (kantor pelayanan pajak, KPP) throughout the
+country for allocation to taypayers.
 
-C<C> is a check digit. It is apparently using Luhn (modulus 10)
-algorithm on the first 9 digits on the NPWP.
+C<C> is a check digit. It is apparently using Luhn (modulus 10) algorithm on the
+first 9 digits on the NPWP.
 
 C<OOO> is a 3-digit local tax office code (kode KPP).
 
-C<BBB> is a 3-digit branch code. C<000> means the taxpayer is the sole
-branch (or, for individuals, the head of the family). C<001>, C<002>,
-and so on denote each branch.
-
-=cut
-
-use warnings;
-use strict;
-use DateTime;
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT = qw(validate_npwp);
-
-=head1 METHODS
-
-=head2 new $str
-
-Create a new C<Business::ID::NPWP> object.
-
-=cut
-
-sub new {
-    my ($class, $str) = @_;
-    bless {
-	_str => $str,
-	_err => undef, # errstr
-	_res => undef, # validation result cache
-    }, $class;
-}
-
-=head2 validate()
-
-Return true if NPWP is valid, or false if otherwise. In the case of NPWP
-being invalid, you can call the errstr() method to get a description
-of the error.
-
-=cut
-
-sub validate {
-    my ($self, $another) = @_;
-    return validate_npwp($another) if $another;
-    return $self->{_res} if defined($self->{_res});
-
-    $self->{_res} = 0;
-    for ($self->{_str}) {
-	s/^\s+//;
-	# assume A = 0 if not specified
-	if (/^\d\./) { $_ = "0$_" }
-	s/\D+//g;
-	# assume BBB = 000 if not specified
-	if (length == 12) { $_ .= "000" }
-	if (length != 15) {
-	    $self->{_err} = "not 15 digit";
-	    return;
-	}
-	/^(.)(.)(.)(.)(.)(.)(.)(.)(.)/;
-	if ((_z(1*$1) + _z(2*$2) + _z(1*$3) +
-	     _z(2*$4) + _z(1*$5) + _z(2*$6) +
-	     _z(1*$7) + _z(2*$8) + _z(1*$9)) % 10) {
-	    $self->{_err} = "wrong check digit";
-	    return;
-	}
-	/^..(\d{6})/;
-	if ($1 < 1) {
-	    $self->{_err} = "serial starts from 1, not 0";
-	    return;
-	}
-    }
-    $self->{_res} = 1;
-}
-
-=head2 errstr()
-
-Return validation error of NPWP, or undef if no error is found. See
-C<validate()>.
-
-=cut
-
-sub errstr {
-    my ($self) = @_;
-    $self->validate and return;
-    $self->{_err};
-}
-
-=head2 normalize()
-
-Return formatted NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub normalize {
-    my ($self, $another) = @_;
-    return Business::ID::NPWP->new($another)->normalize if $another;
-    $self->validate or return;
-    $self->{_str} =~ /^(..)(...)(...)(.)(...)(...)/;
-    "$1.$2.$3.$4-$5.$6";
-}
-
-=head2 pretty()
-
-Alias for normalize().
-
-=cut
-
-sub pretty { normalize(@_) }
-
-=head2 taxpayer_code()
-
-Return 2-digit taxpayer code component of NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub taxpayer_code {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^(..)/;
-    $1;
-}
-
-=head2 kode_wajib_pajak()
-
-Alias for taxpayer_code().
-
-=cut
-
-sub kode_wajib_pajak { taxpayer_code(@_) }
-
-=head2 kode_wp()
-
-Alias for taxpayer_code().
-
-=cut
-
-sub kode_wp { taxpayer_code(@_) }
-
-=head2 serial()
-
-Return 6-digit serial component of NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub serial {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^\d{2}(......)/;
-    $1;
-}
-
-=head2 check_digit()
-
-Return check digit component of NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub check_digit {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^\d{8}(.)/;
-    $1;
-}
-
-=head2 local_tax_office_code()
-
-Return 3-digit local tax office code component of NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub local_tax_office_code {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^\d{9}(...)/;
-    $1;
-}
-
-=head2 kode_kpp()
-
-Alias for local_tax_office_code().
-
-=cut
-
-sub kode_kpp { local_tax_office_code(@_) }
-
-=head2 branch_code()
-
-Return 3-digit branch code component of NPWP, or undef if NPWP is invalid.
-
-=cut
-
-sub branch_code {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^\d{12}(...)/;
-    $1;
-}
-
-=head2 kode_cabang()
-
-Alias for branch_code().
-
-=cut
-
-sub kode_cabang { branch_code(@_) }
-
-=head1 FUNCTIONS
-
-=head2 validate_npwp($string)
-
-Return true if NPWP is valid, or false if otherwise. If you want to
-know the error details, you need to use the OO version (see the
-C<errstr> method).
-
-Exported by default.
-
-=cut
-
-sub validate_npwp {
-    my ($str) = @_;
-    Business::ID::NPWP->new($str)->validate();
-}
-
-sub _z { $_[0] > 9 ? $_[0]-9 : $_[0] }
-
-1;
+C<BBB> is a 3-digit branch code. C<000> means the taxpayer is the sole branch
+(or, for individuals, the head of the family). C<001>, C<002>, and so on denote
+each branch.
--- t/01-basics.t
+++ t/01-basics.t
@@ -1,24 +1,26 @@
-#!perl -T
+#!perl
 
+use 5.010;
 use strict;
 use warnings;
-use Test::More tests => 16 +10;
 
-use Business::ID::NPWP;
+use Business::ID::NPWP qw(parse_npwp);
+use Test::More 0.98;
 
-ok(!(validate_npwp("") ? 1:0), "procedural style (1)");
-ok((validate_npwp("00.000.001.8-000.000") ? 1:0), "procedural style (2)");
+test_parse(
+    npwp   => "02.183.241.5-000.000",
+    status => 200,
+    result => {
+        'branch_code' => '000',
+        'check_digit' => '5',
+        'normalized' => '02.183.241.5-000.000',
+        'serial' => 183241,
+        'tax_office_code' => '000',
+        'taxpayer_code' => '02',
+    },
+);
 
-isa_ok(Business::ID::NPWP->new(""), "Business::ID::NPWP", "new() works on invalid NPWP");
-
-ok(Business::ID::NPWP->new("00.000.001.8-000.000")->validate, "valid NPWP (1)");
-ok(Business::ID::NPWP->new("0.000.001.8-000.000")->validate, "valid NPWP (2, first digit optional)");
-ok(Business::ID::NPWP->new("00.000.001.8-000")->validate, "valid NPWP (3, branch code optional)");
-
-ok(!Business::ID::NPWP->new("00.000.000.8-000.000")->validate, "invalid NPWP: zero serial");
-
-my $i = 0;
-my @npwp = qw(
+my @valid_npwps = qw(
 02.183.241.5-000.000
 02.061.179.4-000.000
 01.957.716.2-000.000
@@ -30,21 +32,25 @@ my @npwp = qw(
 01.233.075.9-000.000
 01.000.724.3.000.000
 );
-for (@npwp) {
-    $i++;
-    ok(Business::ID::NPWP->new($_)->validate, "check digit checking ($i)");
+for (@valid_npwps) {
+    test_parse(npwp => $_, status => 200);
 }
 
-is(Business::ID::NPWP->new("12.000.001.3-000.000")->taxpayer_code,    "12", "taxpayer_code");
-is(Business::ID::NPWP->new("12.000.001.3-000.000")->kode_wajib_pajak, "12", "taxpayer_code (alias, kode_wajib_pajak)");
-is(Business::ID::NPWP->new("12.000.001.3-000.000")->kode_wp,          "12", "taxpayer_code (alias, kode_wp)");
+DONE_TESTING:
+done_testing;
 
-is(Business::ID::NPWP->new("00.000.001.8-000.000")->check_digit, "8", "check_digit");
-
-is(Business::ID::NPWP->new("00.000.001.8-004.000")->local_tax_office_code, "004", "local_tax_office_code");
-is(Business::ID::NPWP->new("00.000.001.8-004.000")->kode_kpp,              "004", "local_tax_office_code (alias, kode_kpp)");
-
-is(Business::ID::NPWP->new("00.000.001.8-000.005")->branch_code, "005", "branch_code");
-is(Business::ID::NPWP->new("00.000.001.8-000.005")->kode_cabang, "005", "branch_code (kode_cabang)");
-
-is(Business::ID::NPWP->new("00 000 001 8 000 000")->normalize, "00.000.001.8-000.000", "normalize");
+sub test_parse {
+    my %args = @_;
+    subtest +($args{name} //= "npwp $args{npwp}"), sub {
+        my $res = parse_npwp(npwp => $args{npwp});
+        if (exists $args{status}) {
+            is($res->[0], $args{status}) or diag explain $res;
+        }
+        if (exists $args{result}) {
+            is_deeply($res->[2], $args{result}) or diag explain $res;
+        }
+        if ($args{posttest}) {
+            $args{posttest}->($res);
+        }
+    };
+}
